45e6286069f624590d31860844d0b8ebc39286a2
Mincheol Shin <shinmc9@gmail.com>
2024-10-13 23:27:37 +0900

    freshman first commit

--- test.py
+++ test.py
@@ -1,4 +1,8 @@
 if __name__ == "__main__":
     print("git test")
     print("charge git")
+
+    print("this is fresh man in charge")
+    print("hello")
+    print("i am making my first project")
     
